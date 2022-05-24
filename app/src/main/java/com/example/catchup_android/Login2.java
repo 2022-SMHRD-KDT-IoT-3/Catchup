4b87d4aa0276472010595dbb2e5b7007a64c3952
jaegyeong99 <kor705@naver.com>
2022-05-24 17:50:42 +0900

    loading, login, join view complete

---
+++ app/src/main/java/com/example/catchup_android/Login2.java
@@ -0,0 +1,17 @@
+package com.example.catchup_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class Login2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+
+        Log.v("activity","loginActivity");
+    }
+}
