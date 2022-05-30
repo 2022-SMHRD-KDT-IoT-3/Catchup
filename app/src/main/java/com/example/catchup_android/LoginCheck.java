80cc562838c1a844588c75dd1ef8775322c6ce57
foxmanRealms <nyang42@naver.com>
2022-05-30 10:27:29 +0900

    220530 fixing
    fix

---
+++ app/src/main/java/com/example/catchup_android/LoginCheck.java
@@ -0,0 +1,6 @@
+package com.example.catchup_android;
+
+public class LoginCheck {
+    // 로그인정보를 계속해서 저장하고 있는 변수
+    public static UserVo info;
+}
---
+++ app/src/main/java/com/example/catchup_android/UserVo.java
@@ -0,0 +1,86 @@
+package com.example.catchup_android;
+
+import java.io.Serializable;
+
+public class UserVo implements Serializable {
+
+    private String id;
+    private String pw;
+    private String name;
+    private String nick;
+    private String mail;
+    private String serial;
+
+    @Override
+    public String toString() {
+        return "UserVo{" +
+                "id='" + id + '\'' +
+                ", pw='" + pw + '\'' +
+                ", name='" + name + '\'' +
+                ", nick='" + nick + '\'' +
+                ", mail='" + mail + '\'' +
+                ", serial='" + serial + '\'' +
+                '}';
+    }
+
+    public UserVo() {
+
+    }
+
+    public UserVo(String id, String pw, String name, String nick, String mail, String serial) {
+        this.id = id;
+        this.pw = pw;
+        this.name = name;
+        this.nick = nick;
+        this.mail = mail;
+        this.serial = serial;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+}
