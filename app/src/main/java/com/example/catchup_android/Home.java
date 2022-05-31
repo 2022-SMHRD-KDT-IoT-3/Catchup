1e38e75d0393d31f6f6e652bb58f7f2e1d8a7a4c
jaegyeong99 <kor705@naver.com>
2022-05-31 15:59:44 +0900

    monitoring page

--- app/src/main/java/com/example/catchup_android/Home.java
+++ app/src/main/java/com/example/catchup_android/Home.java
@@ -10,12 +10,6 @@ import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
 
-    private TextView tv_result_home;
-
-    private Button btn_toLogin, btn_toMyP;
-
-    UserVo vo= new UserVo();
-    UserVo info  =LoginCheck.info;
 
 
     @Override
@@ -23,37 +17,7 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        tv_result_home=findViewById(R.id.tv_result_home);
-        btn_toLogin=findViewById(R.id.btn_toLogin);
 
-        btn_toMyP=findViewById(R.id.btn_toMyP);
-
-
-
-        if (vo !=null){
-            tv_result_home.setText(info.toString());
-        }
-
-
-
-        btn_toLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),Login2.class );
-                startActivity(intent);
-                finish();
-            }
-        });
-
-
-        btn_toMyP.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),Mypage.class );
-                startActivity(intent);
-                finish();
-            }
-        });
 
 
     }
--- app/src/main/java/com/example/catchup_android/Monitoring.java
+++ app/src/main/java/com/example/catchup_android/Monitoring.java
@@ -32,43 +32,11 @@ public class Monitoring extends AppCompatActivity {
 
 
 
-    private TextView tv_id_monit;
-    private Button btn_insertMonint, btn_logout;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monitoring);
 
-        tv_id_monit= findViewById(R.id.tv_id_monit);
-
-        btn_insertMonint= findViewById(R.id.btn_insertMonit);
-        btn_logout= findViewById(R.id.btn_logout);
-
-        //로그아웃 기능
-        btn_logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                LoginCheck.logout();
-
-                Intent intent = new Intent(getApplicationContext(),Login2.class );
-                startActivity(intent);
-                finish();
-
-                Toast.makeText(getApplicationContext() , "로그아웃 완료",Toast.LENGTH_SHORT).show();
-            }
-        });
-        
-        // 모니터링 데이터 입력
-        btn_insertMonint.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                //Log.v("resultValue","모니터 입력대기");
-
-
-            }
-        });
 
 
     }
